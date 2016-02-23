8f26e62e694fed5adbe8a8ee50e468b2857b7d82
Jake Overall <joverall22@gmail.com>
2016-02-23 10:07:43 -0700

    basic setup

---
+++ app/music-controller.js
@@ -0,0 +1,25 @@
+app.controller('MusicController', function($scope){
+    $scope.test = "Music";
+    
+    $scope.boxes = [
+        {note: "A", wave: ""},
+        {note: "A#", wave: ""},
+        {note: "B", wave: ""},
+        {note: "C", wave: ""},
+        {note: "C#", wave: ""},
+        {note: "D", wave: ""},
+        {note: "D#", wave: ""},
+        {note: "E", wave: ""},
+        {note: "F", wave: ""},
+        {note: "F#", wave: ""},
+        {note: "G", wave: ""},
+        {note: "G#", wave: ""},
+        {note: "rest", wave: ""}];
+      
+     $scope.playNote = function(box){
+         
+         
+     }  
+       
+        
+})
---
+++ readme.md
@@ -0,0 +1,17 @@
+Music Boxes
+===========
+
+Requirements
+
+-   There are 12 Notes in a scale
+-   12 boxes one representing each note.
+-   When I hover over a box I want the note to play sound
+-   I want a record button that will track the notes played in order until I press stop
+-   Then Pressing play will replay those notes
+-   Create a box that emits no sound to help with timming.
+
+Bonus Features
+--------------
+-   Simon Says
+-   RockBand Style Notes
+-   Magic Piano App
